0d379204cca0d53fa371c7fadf800ba05d4dc00c
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-03 17:32:23 -0500

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while(i >= 0):
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = []
+    for index in int_list:
+        square = index ** 2
+        squares.append(square)
+    return squares
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
